aa3625c269ad14d6fd81a85394073ee5a9af2341
ivanGonzalezGit <123334540+ivanGonzalezGit@users.noreply.github.com>
2025-07-21 16:28:05 -0300

    Update rutaProtegida.jsx

--- src/components/rutaProtegida.jsx
+++ src/components/rutaProtegida.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuthContext } from '../context/AuthContext';
+import { useAuthContext } from '../context/authContext';
 
 export default function RutaProtegida({isAuthenticated, children})
 {
