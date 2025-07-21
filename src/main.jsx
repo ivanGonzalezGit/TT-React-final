8248c4897ac9f10c4b3cc33fafa44d23e79d0f5d
ivanGonzalezGit <123334540+ivanGonzalezGit@users.noreply.github.com>
2025-07-21 16:21:23 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
-import { AuthProvider } from './context/AuthContext.jsx';
+import { AuthProvider } from './context/authContext.jsx';
 import { TotalProvider } from './context/TotalContext';
 
 
